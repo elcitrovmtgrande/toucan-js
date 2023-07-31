39fcb1a83be1f69843f59a5ae68663c745132f35
Victor MAHE <victormahe@ODMAC01598.local>
2023-07-31 16:31:52 +0200

    Update index

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const Toucan = require('./dist-min/Toucan.min');
+const { Toucan } = require('./dist/Toucan');
 
 const toucan = new Toucan('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJiaXJkIjoiVG91Y2FuICIsImlhdCI6MTY5MDgxMzA1OCwiZXhwIjoxNjkwODE2NjU4fQ.1wDvQNja7P63vP9xY53bAqFVIryb_sAYOxdchlZYg-8');
 
